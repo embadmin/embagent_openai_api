07e3e285026f7f62dce210169b8522a1860b5f5f
vinarajaa <vina.embagent@gmail.com>
2025-05-23 15:44:30 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://www.embagent.com", "http://localhost:3000"]
+    allow_origins=["https://www.embagent.com", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
